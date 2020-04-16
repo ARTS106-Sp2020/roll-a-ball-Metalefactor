94202105da33152312e11e1bf4f7c3afd5c6ca6f
Matt Perrotti <60189116+Metalefactor@users.noreply.github.com>
2020-04-16 02:30:49 -0400

    Q1 - Title Screen

---
+++ Assets/Scripts/UIManager.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class UIManager : MonoBehaviour
+{
+  public Animator startButton;
+  public Animator instructionsButton;
+  public Animator exitButton;
+  public Animator dialog;
+  public Animator contentPanel;
+
+  public void OpenSettings()
+{
+    startButton.SetBool("isHidden", true);
+    exitButton.SetBool("isHidden", true);
+    instructionsButton.SetBool("isHidden", true);
+    dialog.SetBool("isHidden", false);
+}
+
+public void CloseSettings()
+{
+    startButton.SetBool("isHidden", false);
+    exitButton.SetBool("isHidden", false);
+    instructionsButton.SetBool("isHidden", false);
+    dialog.SetBool("isHidden", true);
+}
+
+public void StartGame()
+    {
+        SceneManager.LoadScene("Roll-a-ball");
+    }
+
+    public void CloseGame()
+    {
+      Application.Quit();
+    }
+}
